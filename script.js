d8d60f9fed7d6bb6e1a1322823c9326472138f91
Kritarat.M <ideal141148@gmail.com>
2024-06-30 03:31:18 +0700

    finish games slide

---
+++ script.js
@@ -0,0 +1,23 @@
+const min = (a,b) => {
+    return ( a < b ? a : b );
+}
+const max = (a,b) => {
+    return ( a > b ? a : b );
+}
+
+$(".scroll-right").click( function(dir){
+    const games = $(this).siblings(".scroll-game");
+    const currX = parseInt($(games).css('translate'));
+    const games_width = parseInt($(games).css('width'));
+    const To = (currX - 1200 > -games_width+200 ? currX - 1200 : currX);
+    $(games).css('translate', To + 'px');
+    console.log(To);
+});
+
+$(".scroll-left").click( function(dir){
+    const games = $(this).siblings(".scroll-game");
+    const currX = parseInt($(games).css('translate'));
+    const To = min(currX + 1200, 27);
+    $(games).css('translate', To + 'px');
+    console.log(To);
+});
